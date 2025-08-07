20ff71c57ee9d08f1531343c50374bcd0ceeff24
webz <zilvism@gmail.com>
2025-08-07 08:30:11 +0300

    move boxes y130px

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const object=document.getElementById('rut');
+const goods=document.getElementsByClassName('in');
 
-object.addEventListener('click', (e)=> {
-    e.stopPropagation();
+object.addEventListener('click', ()=> {
     object.classList.toggle('move');
     console.log('clclc')
-})
+});
+
+goods.addEventListener('click', ()=>{
+    goods.classList.toggle('.onto');
+    console.log('ononoo')
+});
